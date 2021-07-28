204e6c888d8377e6199464275578f0592dde6504
Munir Suleman <munirsuleman@hotmail.com>
2021-07-28 09:51:27 +0100

    Add files via upload

---
+++ FalloutTerminal.java
@@ -0,0 +1,255 @@
+package com.bham.pij.assignments.rps;
+
+import java.util.ArrayList;
+
+/**
+ * This FalloutTerminal program       and is part of
+ * Assignment 1 for the OOP/SW1 module. I love Fallout!
+ * 
+ * @author Ian Kenny, Munir Suleman
+ * @version 1.0
+ * @since 2020-10-15
+ * 
+ */
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class FalloutTerminal {
+
+	/**
+	 * This method creates a single line of the terminal display.
+	 * @param startAddress The first address of the line.
+	 * @param wordProbability If less than or equal to 0.6 the line should contain one word.
+	 * @return The line for display.
+	 */
+	public String buildDisplayLine(int startAddress, float wordProbability) {
+
+		/*
+		 *  Add your code here. When you have selected a random word
+		 *  to add to the display, you must call addSelectedWord() to 
+		 *  save that word. For example, if your variable holding the word is
+		 *  called `word' you would call as follows:
+		 * 	addSelectedWord(word);
+		 * 
+		 */
+		
+		StringBuilder oneLine = new StringBuilder();
+		
+		int a;
+		char letter;
+		StringBuilder allChar = new StringBuilder();
+		//creat string of ascii characters from 33-126
+		for(a = 33; a <= 126; a++)
+	      {
+	        letter = (char)a;
+	        allChar.append(letter);
+	      }
+		//remove the number, non cap and cap letters from the string leaving only special characters
+		StringBuilder allChar1 = allChar.delete(15, 25);
+		StringBuilder allChar2 = allChar.delete(22, 48);
+		StringBuilder specChar = allChar.delete(28, 54);
+		
+		// Randomly generate a number and pick the character corresponding to that number from the special string
+		Random r = new Random();
+		int index = r.nextInt(specChar.length());
+		char c = specChar.charAt(index);
+		StringBuilder randomString1 = new StringBuilder();
+		randomString1 = randomString1.append(c);
+		// do last step to create a string of 30 random characters
+		for(int i = 1;i <= 74;i++)
+		{
+			index = r.nextInt(specChar.length());
+			c = specChar.charAt(index);
+			randomString1 = randomString1.append(c);
+		}
+		//full string of random characters
+		oneLine = oneLine.append(randomString1);
+		//Replace the first few characters with the first address converted to Hex
+		String hexStartAddress = Integer.toHexString(startAddress);
+		int otherAddress = startAddress + 240; //240 because 30*8bits
+		
+		if(hexStartAddress.length()>=5)
+			hexStartAddress = hexStartAddress.substring(hexStartAddress.length()-4);
+		
+		String firstAddress = ("0x" + hexStartAddress + " ");
+		oneLine = oneLine.replace(0, 7, firstAddress);
+		
+		//Replace the second address into the string
+		String hexOtherAddress = Integer.toHexString(otherAddress);
+		if(hexOtherAddress.length()>=5)
+			hexOtherAddress = hexOtherAddress.substring(hexOtherAddress.length()-4);
+		
+		String secondAddress = (" 0x" + hexOtherAddress + " ");
+		oneLine = oneLine.replace(37, 45, secondAddress);
+		
+		// from 7-29 and 45-67 random number generator
+		Random rand = new Random();
+		int randStart = rand.nextInt(46); //random number from 0-45 inclusive
+		if(randStart <= 22) {
+			randStart = randStart + 7;
+		}
+		else {
+			randStart = randStart + 22;
+		}
+		int randEnd = randStart + 8;
+		//get a random word and place it somewhere in the special characters region if the wordProb <=0.6
+		String word = getRandomWord();
+		addSelectedWord(word);
+		if(wordProbability <= 0.6f) {
+			oneLine = oneLine.replace(randStart, randEnd, word);
+		}
+		//convert from stringbuilder to string
+		String oneLineFinal = oneLine.toString();
+		
+		return oneLineFinal;
+		
+	}
+
+/*
+ ***************************************************************************************	
+ */
+
+	private static final int START_ADDRESS = 0x9380;
+	private static final int DISPLAY_HEIGHT = 20;
+	private static final int NUM_CHARACTERS_PER_ROW = 60;
+	private static final float WORD_PROB_1 = 0.6f;
+	private static final int WORD_LENGTH = 8;
+	private static final int NUM_GUESSES_ALLOWED = 4;
+	private static final int BYTE_SIZE = 8;
+	private static Random rand;
+	private static final String[] words = {
+			"flourish",
+			"appendix",
+			"separate",
+			"unlawful",
+			"platform",
+			"shoulder",
+			"marriage",
+			"attitude",
+			"reliable",
+			"contempt",
+			"prestige",
+			"evaluate",
+			"division",
+			"birthday",
+			"orthodox",
+			"appetite",
+			"perceive",
+			"pleasant",
+			"surprise",
+			"elephant",
+			"incident",
+			"medieval",
+			"absolute",
+			"dominate",
+			"designer",
+			"misplace",
+			"possible",
+			"graduate",
+			"solution",
+			"governor"
+	};
+	
+	private static String password;
+
+	private static ArrayList<String> selectedWords = new ArrayList<String>();
+	
+	private void run() {
+		
+		System.out.println(buildDisplay());
+		
+		Scanner in = new Scanner(System.in);
+		
+		int guessCount = NUM_GUESSES_ALLOWED;
+		
+		boolean done = false;
+		
+		do {
+			System.out.println("> Password required.");
+			System.out.println("> Attempts remaining = " + guessCount);
+			System.out.println(">");
+			String guess = in.nextLine();
+			
+			if (guess.equalsIgnoreCase(getPassword())) {
+				System.out.println("> Access granted.");
+				done = true;
+			}
+			
+			else {
+				--guessCount;
+				System.out.println("> Access denied.");
+				System.out.println("> Likeness = " + getCorrectCount(guess, password));
+			}	
+		} while (guessCount > 0 && !done);
+		
+		if (guessCount == 0) {
+			System.out.println("> Initiating lockout");
+		}
+		
+		in.close();
+	}
+	
+	private int getCorrectCount(String guess, String password) {
+		
+		int count = 0;
+		
+		for (int i = 0; i < guess.length(); i++) {			
+			if (guess.charAt(i) == password.charAt(i)) {
+				count++;
+			}
+		}
+		
+		return count;
+	}
+		
+	private String getRandomWord() {
+		return words[rand.nextInt(words.length)];
+	}
+	
+	private String buildDisplay() {
+		
+		String ret = "";
+		
+		int address = START_ADDRESS;
+						
+		for (int i = 0; i < DISPLAY_HEIGHT; i++) {
+											
+			float rf = rand.nextFloat();
+			
+			String line = buildDisplayLine(address, rf);
+
+			ret += line + "\n";
+			
+			address += BYTE_SIZE * NUM_CHARACTERS_PER_ROW;
+		}
+		
+		setRandomPassword();
+		
+		return ret;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String pw) {
+		password = pw;
+	}
+
+	private void addSelectedWord(String word) {
+		selectedWords.add(word);
+	}
+	
+	public void setRandomPassword() {
+		password = selectedWords.get(rand.nextInt(selectedWords.size())).toLowerCase();
+	}
+
+	public FalloutTerminal() {
+		rand = new Random(System.currentTimeMillis());	
+	}
+	
+	public static void main(String[] args) {
+		new FalloutTerminal().run();
+	}
+}
---
+++ RockPaperScissors.java
@@ -0,0 +1,266 @@
+package com.bham.pij.assignments.rps;
+
+/**
+ * This RockPaperScissors program plays the Rock, Paper, Scissors game and is part of
+ * Assignment 1 for the OOP/SW1 module
+ * 
+ * @author Ian Kenny, Munir Suleman
+ * @version 1.0
+ * @since 2020-10-15
+ * 
+ */
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class RockPaperScissors {
+	
+	/**
+	 * This method checks that the parameter 'turn' represents a valid turn. A valid turn is 
+	 * in the range 1 to 3 inclusive. A value of 1 means ROCK; a value of 2 means PAPER and a value of 
+	 * 3 means SCISSORS. Each turn value is, therefore, represented as an integer. Any value of 
+	 * 'turn' outside of this range is invalid.
+	 * @param turn The turn to check.
+	 * @return true if the turn is valid, false if is not.
+	 */
+	public boolean isValidTurn(int turn) {
+	
+		/*
+		 * Add your code for this method here./
+		 */
+		
+		boolean val;
+		// if turn is between 1 and 3 inclusive, then return true, else false
+		if(turn >= 1 && turn <= 3)
+			val = true;
+		else 
+			val = false;
+		
+		return val;
+	}
+
+	/**
+	 * This method effectively converts the keyboard input into a valid turn. The keyboard
+	 * input is in the form of a String and will represent the key pressed by the player.
+	 * The player presses 'r' for rock, 'p' for paper and 's' for scissors. This method converts
+	 * that input into a valid turn, represented as an integer in the range 1-3. If the parameter
+	 * 'input' does not contain 'r' or 'p' or 's' this method should return the value -1.
+	 * @param input The turn selected by the player. Options described above.
+	 * @return The turn as an integer. 1 means rock, 2 means paper, 3 means scissors. A return 
+	 * value of -1 means that the parameter was invalid.
+	 */
+	public int getPlayerTurn(String input) {
+		
+		/*
+		 * Add your code for this method  here.
+		 */
+		
+		// if input equals r, return 1. if p, return 2. if s, return 3. else return -1
+		int turn;
+		if(input.equals("r"))
+			turn = 1;
+		else if(input.equals("p"))
+			turn = 2;
+		else if(input.equals("s"))
+			turn = 3;
+		else
+			turn = -1;
+			
+		return turn;
+	}
+	
+	/**
+	 * This method decides which of the two players - the player or the computer - has won the round.
+	 * The winner is decided as follows: 
+	 * - rock beats scissors
+	 * - paper beats rock
+	 * - scissors beats paper
+	 * - if the the players choose the same object the round is a draw.
+	 * @param playerChoice The player's turn.
+	 * @param computerChoice The computer's turn.
+	 * @return The winner, according to the rules above. If the player wins the method should
+	 * return 'PLAYER', if the computer wins the method should return 'COMPUTER', and if it's a 
+	 * draw the method should return 'DRAW'.
+	 */
+	public String getWinner(int playerChoice, int computerChoice) {
+		
+		
+		/*
+		 * Add your code for this method  here.
+		 * rock = 1, paper = 2, scissor = 3
+		 */
+		
+		
+		String winner = "DRAW";
+		if(playerChoice == 1 && computerChoice == 2)
+			winner = "COMPUTER"; //paper beats rock
+		else if(playerChoice == 1 && computerChoice == 3)
+			winner = "PLAYER"; //rock beat scissors
+		else if(playerChoice == 2 && computerChoice == 1)
+			winner = "PLAYER"; //paper beats rock
+		else if(playerChoice == 2 && computerChoice == 3)
+			winner = "COMPUTER"; //scissors beats paper
+		else if(playerChoice == 3 && computerChoice == 1)
+			winner = "COMPUTER"; //rock beats scissors
+		else if(playerChoice == 3 && computerChoice == 2)
+			winner = "PLAYER"; //scissors beats paper
+		else
+			winner = "DRAW";
+		
+		return winner;
+	}
+	
+	/**
+	 * This method effectively converts a turn represented as an integer to a String.
+	 * If the parameter has the value 1 the method should return 'ROCK'.
+	 * If the parameter has the value 2 the method should return 'PAPER'.
+	 * If the parameter has the value 3 the method should return 'SCISSORS'.
+	 * @param turn The turn to convert to a String.
+	 * @return The turn as a String.
+	 */
+	public String getTurnAsString(int turn) {
+		
+		/*
+		 * Add your code for this method  here.
+		 */
+		
+		// if 1 return ROCK. if 2 return PAPER. if 3 return SCISSORS.
+		String choice = null;
+		if(turn == 1)
+			choice = "ROCK";
+		else if(turn == 2)
+			choice = "PAPER";
+		else if(turn == 3)
+			choice = "SCISSORS";
+		
+		return choice;	
+	}
+
+/* ************************************************************************************
+
+You do not need to look at the code below this point. If you are curious, however, feel 
+free. Do not change anything below this point though or your own code might not work.
+
+*/
+	
+	private final static int IROCK = 1;
+	private final static int IPAPER = 2;
+	private final static int ISCISSORS = 3;
+	
+	private final static String ROCK_INPUT = "r";
+	private final static String PAPER_INPUT = "p";
+	private final static String SCISSORS_INPUT = "s";
+
+	private final static String ROCK = "Rock";
+	private final static String PAPER = "Paper";
+	private final static String SCISSORS = "Scissors";
+
+	private final static String PLAYER = "PLAYER";
+	private final static String COMPUTER = "COMPUTER";
+	private final static String DRAW = "DRAW";
+	
+	private Random rand = new Random(System.currentTimeMillis());
+	 
+	private int playerWins;
+	private int computerWins; 
+
+	private void run() {
+		
+		System.out.println("Choose rock ('r' key), paper ('p' key) or scissors ('s' key). Press 'x' to quit.");
+		Scanner sc = new Scanner(System.in);
+
+		String splayerTurn = sc.nextLine();
+		
+		while (!splayerTurn.equals("x")) {
+			
+			int playerTurn = getPlayerTurn(splayerTurn);
+			
+			if (!isValidTurn(playerTurn)) {
+				System.out.println("Invalid input.");
+			}
+			
+			else {
+				
+				int computerTurn = getRandomTurn();
+			
+				System.out.println("The player chose " + getTurnAsString(playerTurn) + " and the computer chose " + getTurnAsString(computerTurn));
+				
+				String result = getWinner(playerTurn, computerTurn);
+				
+				recordWin(result);
+				
+				if (result.equals(DRAW)) {
+					System.out.println("The game is a draw.");
+				}
+				
+				else {
+					System.out.println("The winner is the " + result + ".");
+				}
+			}
+			
+			splayerTurn = sc.nextLine();
+		}
+		
+		sc.close();
+		
+		System.out.println("Quitting.");
+		
+		System.out.println("The player won " + getPlayerWins() + " time(s).");
+		System.out.println("The computer won " + getComputerWins() + " time(s).");		
+	}
+	
+	private int getRandomTurn() {
+		return rand.nextInt(3)+1;
+	}
+	
+	/**
+	 * This method records a win for either the player or the computer based on the parameter 'result'. 
+	 * If the value of the parameter is 'PLAYER' the method increments the number of player wins.
+	 * If the value of the parameter is 'COMPUTER' the method increments the number of computer wins.
+	 * @param result the player who has won. Either 'PLAYER' or 'COMPUTER'.
+	 */
+	private void recordWin(String result) {
+
+		if (result.equals(PLAYER)) {
+			addPlayerWin();
+		}
+
+		if (result.equals(COMPUTER)) {
+			addComputerWin();
+		}
+	}
+	
+	/**
+	 * This method returns the number of times the player has won.
+	 * @return The number of times the player has won.
+	 */
+	private int getPlayerWins() {
+		return playerWins;
+	}
+	
+	/**
+	 * This method returns the number of times the computer has won.
+	 * @return The number of times the computer has won.
+	 */
+	private int getComputerWins() {
+		return computerWins;
+	}
+	
+	/**
+	 * This method adds 1 to the number of player wins.
+	 */
+	private void addPlayerWin() {
+		++playerWins;
+	}
+	
+	/**
+	 * This method adds 1 to the number of computer wins.
+	 */
+	private void addComputerWin() {
+		++computerWins;
+	}	
+	
+	public static void main(String[] args) {
+		new RockPaperScissors().run();
+	}
+}
